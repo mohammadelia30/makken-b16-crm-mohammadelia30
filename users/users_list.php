41d63cf12a47affea69cc61f7b6bba32691836d5
mohammad <mohammadelia330@gmail.com>
2024-03-08 23:52:18 +0330

    fix navbar in 3 pages

--- users/users_list.php
+++ users/users_list.php
@@ -19,37 +19,24 @@
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" href="#">کاربران</a>
         <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="./user_list.html">لیست کاربران</a></li>
-          <li><a class="dropdown-item" href="./user_add.html">افزودن کاربر</a></li>
+          <li><a class="dropdown-item" href="./users_list.php">لیست کاربران</a></li>
+          <li><a class="dropdown-item" href="./users_add.php">افزودن کاربر</a></li>
         </ul>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" href="#">محصولات</a>
         <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="../product/product_list.html">لیست محصولات</a></li>
-          <li><a class="dropdown-item" href="../product/product_add.html">افزودن محصول</a></li>
+          <li><a class="dropdown-item" href="../product/product_list.php">لیست محصولات</a></li>
+          <li><a class="dropdown-item" href="../product/product_add.php">افزودن محصول</a></li>
         </ul>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" href="#">سفارشات</a>
         <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="../order/order_list.html">لیست سفارشات</a></li>
-          <li><a class="dropdown-item" href="../order/order_list.html">افزودن سفارش</a></li>
+          <li><a class="dropdown-item" href="../order/order_list.php">لیست سفارشات</a></li>
+          <li><a class="dropdown-item" href="../order/order_list.php">افزودن سفارش</a></li>
         </ul>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" href="#"><svg
-                xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-grid-fill" viewBox="0 0 16 16">
-                <path
-                    d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5z" />
-            </svg></a>
-        <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">دیجیتال</a></li>
-            <li><a class="dropdown-item" href="#"></a>پوشاک</li>
-            <li><a class="dropdown-item" href="#"></a>نوشت افزار</li>
-        </ul>
-    </li>
       <li class="nav-item">
         <a class="nav-link bg-danger text-white mynav" href="../login/login.html">ورود/ثبت نام</a>
       </li>
