430059145298866f9ad8a3e0acc4c79cdce294e7
mohammad <mohammadelia330@gmail.com>
2024-03-09 15:17:46 +0330

    create add user page

--- users/users_add.php
+++ users/users_add.php
@@ -12,16 +12,16 @@
 
 <body class="bg-dark">
   <div class="login-div">
-    <form action="/action_page.php" class="was-validated">
+    <form action="../index.php?type=Add&own=users" method="post" class="was-validated">
       <div class="mb-3 mt-3">
         <label for="uname" class="form-label text-white">نام:</label>
-        <input type="text" class="form-control" id="uname" placeholder="نام" name="fname" required>
+        <input type="text" class="form-control" id="uname" placeholder="نام" name="first_name" required>
         <div class="valid-feedback">درست</div>
         <div class="invalid-feedback">این فیلد اجباری</div>
       </div>
       <div class="mb-3">
         <label for="pwd" class="form-label text-white">نام خانوادگی:</label>
-        <input type="text" class="form-control" id="pwd" placeholder="نام خانوادگی" name="lname" required>
+        <input type="text" class="form-control" id="pwd" placeholder="نام خانوادگی" name="last_name" required>
         <div class="valid-feedback">درست</div>
         <div class="invalid-feedback">این فیلد اجباری</div>
       </div>
@@ -42,29 +42,30 @@
       <h4 class="text-white">وضعیت:</h4>
       <br>
       <div class="form-check">
-        <input type="radio" class="form-check-input" id="radio1" name="status-n" value="normal" checked>
+        <input type="radio" class="form-check-input" id="radio1" name="status" value="عادی" checked>
         <label class="form-check-label text-white" for="radio1">عادی</label>
       </div>
       <div class="form-check">
-        <input type="radio" class="form-check-input" id="radio2" name="status-p" value="primary">
+        <input type="radio" class="form-check-input" id="radio2" name="status" value="ویژه">
         <label class="form-check-label text-white" for="radio2">ویژه</label>
       </div>
       <br>
       <h4 class="text-white">جنسیت:</h4>
       <br>
       <div class="form-check">
-        <input type="radio" class="form-check-input" id="radio1" name="gender-m" value="mail" checked>
-        <label class="form-check-label text-white" for="radio1">عادی</label>
+        <input type="radio" class="form-check-input" id="radio1" name="gender" value="آقا" checked>
+        <label class="form-check-label text-white" for="radio1">آقا</label>
       </div>
       <div class="form-check">
-        <input type="radio" class="form-check-input" id="radio2" name="gender-f" value="femail">
-        <label class="form-check-label text-white" for="radio2">ویژه</label>
+        <input type="radio" class="form-check-input" id="radio2" name="gender" value="خانم">
+        <label class="form-check-label text-white" for="radio2">خانم</label>
       </div>
       <br>
       <label for="comment1" class="text-white">آدرس:</label>
       <textarea class="form-control" rows="5" id="comment1" name="address" placeholder="آدرس"></textarea>
       <br>
       <label for="comment1" class="text-white">توضیحات:</label>
+      <textarea class="form-control" rows="5" id="comment1" name="comment" placeholder="توضیحات"></textarea>
       <br>
       <button type="submit" class="btn btn-outline-success">ذخیره</button>
     </form>
