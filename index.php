036c1f0635a908d9a3e88fbdac1d7dd5b1615d2e
mohammad <mohammadelia330@gmail.com>
2024-03-09 16:37:16 +0330

    crate edit process

--- index.php
+++ index.php
@@ -1,31 +1,47 @@
 <?php
-$conn = mysqli_connect("localhost","root","","crm");
+$conn = mysqli_connect("localhost", "root", "", "crm");
 if (mysqli_connect_errno()) {
     printf("connaction faild", mysqli_connect_error());
     exit();
 }
 $type = $_GET["type"];
 $table = $_GET["own"];
-if ( $type == "Add") {
+if ($type == "Add") {
     $endk = array_key_last($_POST);
     $query = "INSERT INTO $table (";
-    foreach($_POST as $key => $value) {
+    foreach ($_POST as $key => $value) {
         $query .= $key;
         if ($key !== $endk) {
             $query .= ",";
-        }else{
-            $query .=")";
+        } else {
+            $query .= ")";
         }
     }
-        $query .= "VALUES (";
-        foreach($_POST as $key => $value){
-            if($key !== $endk){
-                $query .= "'$value',";
-            }else{
-                $query .= "'$value');";
+    $query .= "VALUES (";
+    foreach ($_POST as $key => $value) {
+        if ($key !== $endk) {
+            $query .= "'$value',";
+        } else {
+            $query .= "'$value');";
         }
     }
     echo $query;
-    mysqli_query($conn,$query);
-    header("Location: ./$table/"."$table"."_list.php");
+    mysqli_query($conn, $query);
+    header("Location: ./$table/" . "$table" . "_list.php");
+} elseif ($type == "Edit") {
+    $id = $_GET["id"];
+    $endk = array_key_last($_POST);
+    $query = "UPDATE $table SET ";
+    foreach ($_POST as $key => $value) {
+        $query .= "$key='$value'";
+        if ($key !== $endk) {
+            $query .= ", ";
+        } else {
+            $query .= " ";
+        }
+    }
+    $query .= "WHERE id=$id;";
+    echo $query;
+    mysqli_query($conn, $query);
+    header("Location:" . "./$table/$table". "_list.php");
 }
