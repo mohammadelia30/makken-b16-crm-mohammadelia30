ccd6c3cd3430b254801506b7a2a35cb8b2017650
mohammad <mohammadelia30@gmail.com>
2024-03-19 16:40:02 +0330

    make edit for Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,19 @@ class UserController extends Controller
 {
     public function create(Request $request)
     {
-        DB::table("users")->insert([
-            "first_name"=> $request->first_name,
+        DB::table("users")->insert([$request->except("_token")]);
+        return redirect("/users/index");
+    }
+    public function edit(Request $request,$id){
+        DB::table('users')->where("id",$id)->update([
+            "first_name" => $request->first_name,
             "last_name"=> $request->last_name,
-            "password"=> $request-> password,
-            "phone_number" => $request->phone_number,
-            "status" => $request->status,
-            "gender" => $request->gender,
+            "password" => $request->password,
+            "phone_number"=> $request->phone_number,
+            "status"=> $request->status,
+            "gender"=> $request->gender,
             "address"=> $request->address,
-            "comment" => $request->comment
+            "comment"=> $request->comment
         ]);
         return redirect("/users/index");
     }
