e374764e0f40374bdee6d9290b164f11f90fcc93
mohammad <mohammadelia30@gmail.com>
2024-03-20 16:00:34 +0330

    create function for controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,4 +11,30 @@ class ProductController extends Controller
         $products = DB::table('products')->get();
         return view('products.index',['products'=> $products]);
     }
+    public function createPage(){
+        return view('products.create')->name('productcreatepage');
+    }
+    public function editPage($id){
+        $product = DB::table('products')->where('id',$id)->first();
+        return view('products.edit',['product'=> $product]);
+    }
+    public function create(Request $request){
+        DB::table('products')->insert([$request->except('_token')]);
+        return redirect()->route('productlist');
+    }
+    public function edit(Request $request,$id){
+        DB::table('products')->where('id',$id)->update([
+            'name'=> $request->name,
+            'price'=> $request->price,
+            'color' => $request->color,
+            'status'=> $request->status,
+            'number'=> $request->number,
+            'comment'=> $request->comment
+        ]);
+        return redirect()->route('productslist');
+    }
+    public function delete($id){
+        DB::table('products')->where('id',$id)->delete();
+        return redirect()->route('productslist');
+    }
 }
