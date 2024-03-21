6f7b506ded7aa36dd5d16ae7851a28f0223af2b3
mohammad <mohammadelia30@gmail.com>
2024-03-21 22:14:31 +0330

    fix disable color input

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -19,40 +19,40 @@
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" href="#">کاربران</a>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="{{route('userslist')}}">لیست کاربران</a></li>
-                    <li><a class="dropdown-item" href="{{route('usercreatepage')}}">افزودن کاربر</a></li>
+                    <li><a class="dropdown-item" href="{{ route('userslist') }}">لیست کاربران</a></li>
+                    <li><a class="dropdown-item" href="{{ route('usercreatepage') }}">افزودن کاربر</a></li>
                 </ul>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" href="#">محصولات</a>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="{{route('productslist')}}">لیست محصولات</a></li>
-                    <li><a class="dropdown-item" href="{{route('productcretepage')}}">افزودن محصول</a></li>
+                    <li><a class="dropdown-item" href="{{ route('productslist') }}">لیست محصولات</a></li>
+                    <li><a class="dropdown-item" href="{{ route('productcretepage') }}">افزودن محصول</a></li>
                 </ul>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" href="#">سفارشات</a>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="{{route('orderslist')}}">لیست سفارشات</a></li>
-                    <li><a class="dropdown-item" href="{{route('ordercreatepage')}}">افزودن سفارش</a></li>
+                    <li><a class="dropdown-item" href="{{ route('orderslist') }}">لیست سفارشات</a></li>
+                    <li><a class="dropdown-item" href="{{ route('ordercreatepage') }}">افزودن سفارش</a></li>
                 </ul>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" href="#">مقالات</a>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="{{route('postslist')}}">لیست مقالات</a></li>
-                    <li><a class="dropdown-item" href="{{route('postcreatepage')}}">افزودن مقاله</a></li>
+                    <li><a class="dropdown-item" href="{{ route('postslist') }}">لیست مقالات</a></li>
+                    <li><a class="dropdown-item" href="{{ route('postcreatepage') }}">افزودن مقاله</a></li>
                 </ul>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" href="#">دسته بندی</a>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="{{route('categureslist')}}">لیست دسته بندی</a></li>
-                    <li><a class="dropdown-item" href="{{route('categutycreatepage')}}">افزودن دسته بندی</a></li>
+                    <li><a class="dropdown-item" href="{{ route('categureslist') }}">لیست دسته بندی</a></li>
+                    <li><a class="dropdown-item" href="{{ route('categutycreatepage') }}">افزودن دسته بندی</a></li>
                 </ul>
             </li>
             <li class="nav-item">
-                <a class="nav-link bg-danger text-white mynav" href="{{route('loginpage')}}">ورود/ثبت نام</a>
+                <a class="nav-link bg-danger text-white mynav" href="{{ route('loginpage') }}">ورود/ثبت نام</a>
             </li>
         </ul>
     </div>
@@ -76,7 +76,7 @@
                     <td>{{ $product->name }}</td>
                     <td>{{ $product->price }}</td>
                     <td><input type="color" class="form-control form-control-color" id="pwd" name="color"
-                            value="{{ $product->color }}" readonly>
+                            value="{{ $product->color }}" disabled>
                     </td>
                     <td>{{ $product->status }}</td>
                     <td>{{ $product->number }}</td>
@@ -84,10 +84,11 @@
                         <textarea class="form-control" rows="5" id="comment1" name="comment" placeholder="توضیحات" readonly>{{ $product->comment }}</textarea>
                     </td>
                     <td>
-                        <form action="{{ route('productdelete',$product->id) }}" method="POST">
+                        <form action="{{ route('productdelete', $product->id) }}" method="POST">
                             @csrf
                             @method('delete')
-                            <button onclick="return confirm('آیا از حذف این مورد اطمینان دارید')" type="submit" class="btn btn-outline-danger">
+                            <button onclick="return confirm('آیا از حذف این مورد اطمینان دارید')" type="submit"
+                                class="btn btn-outline-danger">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                     fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                     <path
@@ -98,9 +99,9 @@
                             </button>
                         </form>
                     </td>
-                    <td><a href="{{ route('producteditpage',$product->id) }}"><button class="btn btn-outline-warning"><svg
-                                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-feather" viewBox="0 0 16 16">
+                    <td><a href="{{ route('producteditpage', $product->id) }}"><button
+                                class="btn btn-outline-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                    height="16" fill="currentColor" class="bi bi-feather" viewBox="0 0 16 16">
                                     <path
                                         d="M15.807.531c-.174-.177-.41-.289-.64-.363a3.8 3.8 0 0 0-.833-.15c-.62-.049-1.394 0-2.252.175C10.365.545 8.264 1.415 6.315 3.1S3.147 6.824 2.557 8.523c-.294.847-.44 1.634-.429 2.268.005.316.05.62.154.88q.025.061.056.122A68 68 0 0 0 .08 15.198a.53.53 0 0 0 .157.72.504.504 0 0 0 .705-.16 68 68 0 0 1 2.158-3.26c.285.141.616.195.958.182.513-.02 1.098-.188 1.723-.49 1.25-.605 2.744-1.787 4.303-3.642l1.518-1.55a.53.53 0 0 0 0-.739l-.729-.744 1.311.209a.5.5 0 0 0 .443-.15l.663-.684c.663-.68 1.292-1.325 1.763-1.892.314-.378.585-.752.754-1.107.163-.345.278-.773.112-1.188a.5.5 0 0 0-.112-.172M3.733 11.62C5.385 9.374 7.24 7.215 9.309 5.394l1.21 1.234-1.171 1.196-.027.03c-1.5 1.789-2.891 2.867-3.977 3.393-.544.263-.99.378-1.324.39a1.3 1.3 0 0 1-.287-.018Zm6.769-7.22c1.31-1.028 2.7-1.914 4.172-2.6a7 7 0 0 1-.4.523c-.442.533-1.028 1.134-1.681 1.804l-.51.524zm3.346-3.357C9.594 3.147 6.045 6.8 3.149 10.678c.007-.464.121-1.086.37-1.806.533-1.535 1.65-3.415 3.455-4.976 1.807-1.561 3.746-2.36 5.31-2.68a8 8 0 0 1 1.564-.173" />
                                 </svg></button></a></td>
