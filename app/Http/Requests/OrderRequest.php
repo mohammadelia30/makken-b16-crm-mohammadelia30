2f65d9254550922276118ece430ae2da3d7d1481
mohammad <mohammadelia30@gmail.com>
2024-03-22 22:20:28 +0330

    make order validation request

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -22,7 +22,27 @@ class OrderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "sname" => "required|min:3|max:50",
+            'gname' => 'required|min:3|max:50',
+            'code' => 'required|min:1|max:10',
+            'date' => 'required',
+            'time' => 'required',
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'sname.required'=> 'نام فرستنده الزامی است',
+            'sname.min'=> 'نام فرستنده کمتر از ۳ حرف است',
+            'sname.max'=> 'نام فرستنده بیش از حد مجاز است',
+            'gname.required'=> 'نام گیرنده الزامی است',
+            'gname.min'=> 'نام گیرنده کمتر از ۳ حرف است',
+            'gname.max'=> 'نام گیرنده بیش از حد مجاز است',
+            'cond.required'=>'کد سفارش الزامی است',
+            'code.min'=> 'کد سفارش باید حداقل شامل یک رقم باشد',
+            'code.max'=> 'کد سفارش بیش از حد مجاز می باشد',
+            'date'=> 'تاریخ تحویل انتخاب نشده است',
+            'time'=> 'زمان دریافت انتخاب نشده است'
         ];
     }
 }
