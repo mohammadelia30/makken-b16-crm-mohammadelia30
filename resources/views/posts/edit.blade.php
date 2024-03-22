12e086e40d6436dad50fa7cbf027270720381493
mohammad <mohammadelia30@gmail.com>
2024-03-22 21:37:20 +0330

    make message for edit post validation

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -14,6 +14,15 @@
   <div class="login-div">
     <form action="{{route('postedit',$post->id)}}" method="POST" class="was-validated">
         @csrf
+        @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         <h1>ویرایش مقاله</h1>
         <br>
       <div class="mb-3 mt-3">
