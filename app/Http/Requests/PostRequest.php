2d2913751daa03bc2a03a373ac71ec3a9b479c17
mohammad <mohammadelia30@gmail.com>
2024-03-22 20:51:16 +0330

    make postrequest

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -11,7 +11,7 @@ class PostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class PostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title'=>'required|min:4|max:10',
+            'auther'=>'required',
+            'content'=> 'required'
         ];
     }
 }
