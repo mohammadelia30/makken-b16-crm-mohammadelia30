d0fcb52f450d8d1422574edfb6831959883cfcac
mohammad <mohammadelia30@gmail.com>
2024-03-22 20:17:53 +0330

    make message for edit product validation

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -14,6 +14,15 @@
   <div class="login-div">
     <form action="{{route('prouctedit',$product->id)}}" method="post" class="was-validated">
         @csrf
+        @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         <h1>ویرایش محصول</h1>
         <br>
       <div class="mb-3 mt-3">
