1904b66843b55df37f3e9fbccdc56053c28f7da5
unknown <mohammadelia30@gmail.com>
2024-06-11 17:20:14 +0330

    edit error for validations

--- app/Http/Requests/LabelRequest.php
+++ app/Http/Requests/LabelRequest.php
@@ -11,7 +11,7 @@ class LabelRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/NoteRequest.php
+++ app/Http/Requests/NoteRequest.php
@@ -11,7 +11,7 @@ class NoteRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
