0c2268771739c33d09e8f78142e37f6a63292a45
unknown <mohammadelia30@gmail.com>
2024-06-11 17:41:11 +0330

    make job for send welcome email

---
+++ app/Jobs/SendMail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    protected $first_name, $last_name, $email;
+    public function __construct($first_name, $last_name,$email)
+    {
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->email = $email;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Mail::to($this->email)->send(new Welcom($this->first_name, $this->last_name));
+    }
+}
